4d0f1873a6a43f5d207656222a76748a7921c970
jubuyer <jahmed10@stuy.edu>
2021-12-26 08:42:15 -0500

    About section framework

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from "../styles/About.css"
+import WTC from "../assets/about/wtc.jpeg"
 
 const About = () => {
     return (
-        <div className='heading'>
-            <h1>About Me</h1>
+        <div className='about-container'>
+            <div className='heading'>
+                <h1>About</h1>
+            </div>
+            <div className='about-block'>
+                <p className='about-body'>
+                    I'm Jubayer Ahmed, undergraduate student at CUNY Hunter College 
+                    majoring in computer science. I am currently working on my
+                    skills in frontend development and working with data. 
+                </p>
+                <div className='nice-image'>
+                    <img src={WTC} className='nice-image' alt='Photography'></img>
+                </div>
+            </div>
         </div>
     )
 }
