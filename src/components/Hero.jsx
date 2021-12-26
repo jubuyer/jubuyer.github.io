a257f8093389ebec45927e6374782634abd49c60
jubuyer <jahmed10@stuy.edu>
2021-12-25 22:06:55 -0500

    frameworks

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React, { Component, useEffect } from 'react';
+import styles from "../styles/Hero.css"
+
+const Navbar = () => {
+    return (
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1>Jubayer Ahmed</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
