8d8da7abae05addbb1b6ff2a4a5cbe9dd0b3d3ad
jubuyer <jahmed10@stuy.edu>
2021-12-26 08:42:32 -0500

    buttons + styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,14 @@ const Navbar = () => {
             <div className='lower-text'>
                 <h1>CUNY Hunter College | Daedalus Honors Scholar | Developer</h1>
             </div>
+            <div>
+                <a href='mailto:jubayer7a@gmail.com' className='contact-button'>Contact Me</a>
+            </div>
+            <svg class="arrows">
+              <path class="a1" d="M0 0 L30 32 L60 0"></path>
+              <path class="a2" d="M0 20 L30 52 L60 20"></path>
+              <path class="a3" d="M0 40 L30 72 L60 40"></path>
+            </svg>
         </div>
     )
 }
