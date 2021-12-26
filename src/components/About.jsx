e6c918e77c4e861e0f0bd703f10165dcd80125a8
jubuyer <jahmed10@stuy.edu>
2021-12-26 10:41:36 -0500

    About section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "../styles/About.css"
 import WTC from "../assets/about/wtc.jpeg"
+import Resume from "../assets/about/AHMED_JUBAYER_RESUME.pdf"
 
 const About = () => {
     return (
@@ -11,13 +12,32 @@ const About = () => {
             <div className='about-block'>
                 <p className='about-body'>
                     I'm Jubayer Ahmed, undergraduate student at CUNY Hunter College 
-                    majoring in computer science. I am currently working on my
-                    skills in frontend development and working with data. 
+                    majoring in computer science. I'm currently working on my
+                    skills in frontend development and processing data. Check out the 
+                    projects section to see what technologies I've worked with.
+                    <br></br>
+                    <br></br>
+                    When not working, I love to play soccer and go running in different areas 
+                    of NYC. I watch professional football and Formula 1 in my free time and take
+                    photos of where I go.
+                    <br></br>
+                    <br></br>
+                    Stay in touch! Contact me through:
+                    <div className='contacts'>
+                        <a href='mailto:jubayer7a@gmail.com' className='contact-buttons'>email</a>
+                        <a href='https://www.linkedin.com/in/jubayera/' className='contact-buttons'>linkedin</a>
+                    </div>
+                    <br></br>
+                    Learn more about what I do here:
+                    <div className='contacts'>
+                        <a href='https://github.com/jubuyer' className='contact-buttons'>github</a>
+                        <a href= {Resume} target = "_blank" className='contact-buttons'>resume</a>
+                    </div>
                 </p>
                 <div className='nice-image'>
                     <img src={WTC} className='nice-image' alt='Photography'></img>
                 </div>
-            </div>
+            </div>     
         </div>
     )
 }
