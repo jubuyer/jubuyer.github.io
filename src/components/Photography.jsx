d281afbe8a151d113765a53d4216631d6c1c8636
jubuyer <jahmed10@stuy.edu>
2022-06-06 21:21:12 -0400

    more changes

--- src/components/Photography.jsx
+++ src/components/Photography.jsx
@@ -6,20 +6,11 @@ import itemData from "../assets/photography/imageInfo"
 
 const Photography = () => {
   return (
-    <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
-    <ImageList variant="masonry" cols={3} gap={8}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  </Box>
+    <div>
+      <p>
+        hi
+      </p>
+    </div>
     );
 };
 
